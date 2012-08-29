25b39cd170c45a927fb5c79469796c3259575bae
Dave Ely <dely@dijatool.com>
2012-08-29 11:11:35 -0700

    Stepping stones.
    Team names and date and times are working now. Next we'll grab the odds
    and then we'll spit the data properly

--- scrape1.py
+++ scrape1.py
@@ -6,6 +6,103 @@ import sys
 
 from BeautifulSoup import BeautifulSoup as bs
 
+_teamNames = {	'Buffalo' :			'Bills',
+				'Miami' :			'Dolphins',
+				'New England' :		'Patriots',
+				'NY Jets' :			'Jets',
+				'Baltimore' :		'Ravens',
+				'Cincinnati' :		'Bengals',
+				'Cleveland' :		'Browns',
+				'Pittsburgh' :		'Steelers',
+				'Indianapolis' :	'Colts',
+				'Houston' :			'Texans',
+				'Jacksonville' :	'Jaguars',
+				'Tennessee' :		'Titans',
+				'Denver' :			'Broncos',
+				'Kansas City' :		'Chiefs',
+				'Oakland' :			'Raiders',
+				'San Diego' :		'Chargers',
+				'Philadelphia' :	'Eagles',
+				'Dallas' :			'Cowboys',
+				'NY Giants' :		'Giants',
+				'Washington' :		'Redskins',
+				'Chicago' :			'Bears',
+				'Detroit' :			'Lions',
+				'Green Bay' :		'Packers',
+				'Minnesota' :		'Vikings',
+				'Atlanta' :			'Falcons',
+				'Carolina' :		'Panthers',
+				'New Orleans' :		'Saints',
+				'Tampa Bay' :		'Buccaneers',
+				'Seattle' :			'Seahawks',
+				'San Francisco' :	'49ers',
+				'St. Louis' :		'Rams',
+				'Arizona' :			'Cardinals',
+		}
+
+
+
+# def leaf( tag ):
+# 	'''
+# 		get text inside <tag><tag>... text
+# 			<td>	<div><font size="2"><i><font color="#FFFFFF"> 
+# 			+ + + + +</font></i></font></div></td>
+# 		<td height="10"><font size="2"> Bierfeuerwerk <strong></strong> </font></td>
+# 	'''
+# 	leafText = ""
+# 	for text in tag.findAll( text=True ) :
+# 		text = text.strip()
+# 		if text :
+# 			leafText = text
+# 
+# 	return leafText
+
+def teamNameTranslate( teamCity ) :
+	'''
+		Translate from the city name to the moniker used by the team
+		
+		If all else fails, fall back to the city data
+	'''
+	return _teamNames.get( teamCity, teamCity )
+
+
+def teamNames( tag, offset ) :
+	'''
+		Get the teamNames tuple
+		
+		It's stuffed inside "odds-row odds-row-odd odds-row-n-1" where n is a game offset
+		and then "game-left"
+
+		eg: Bears @ Browns
+	'''
+	newOffset = offset + 1
+	classId = 'odds-row odds-row-odd odds-row-%s-1' % newOffset
+	namesWrapper = tag.findChild( None, { "class" : classId } )
+	namesObj = namesWrapper.findChild( None, { "class" : "game-left" } )
+	( visitor, home ) = namesObj.findAll( text=True )
+	visitor = teamNameTranslate( visitor )
+	home = teamNameTranslate( home )
+	return visitor, home
+
+
+def dateAndTime( tag, offset ) :
+	'''
+		Get the date and time tuple
+		
+		It's stuffed inside "odds-row odds-row-even odds-row-n-2" where n is a game offset
+		and then "game-left"
+
+		eg: Bears @ Browns
+	'''
+	newOffset = offset + 1
+	classId = 'odds-row odds-row-even odds-row-%s-2' % newOffset
+	namesWrapper = tag.findChild( None, { "class" : classId } )
+	namesObj = namesWrapper.findChild( None, { "class" : "game-left" } )
+	( date, time ) = namesObj.findAll( text=True )
+	return date, time
+
+
+
 
 def download() :
 	'''
@@ -17,21 +114,15 @@ def download() :
 	# opener.addheaders = [("User-agent" , "Mozilla/5.0")]
 	link = opener.open( url )
 	page = link.read()
-
 	soup = bs( page )
-	
-	#games = soup.findAll( None, { "class" : "game" } )
-	#for game in enumerate( games ) :
-	#	print game
-	
+
 	oddsContainer = soup.findChild( None, { "class" : "odds-tables-container" })
-	# print oddsContainer
 	oddsRows = oddsContainer.findAll( None, { "class" : "oddsshark-odds-table allodds-odds" })
-	for game in enumerate( oddsRows ):
-		print game
-		print
-		print "End of game!"
-		print
+	for i, game in enumerate( oddsRows ) :
+		visitor, home = teamNames( game, i )
+		print visitor, "@",  home
+		date, time = dateAndTime( game, i )
+		print date, time
 
 
 if __name__ == '__main__':
