8acebb0ee2655f2e925e825f8345c18c76644707
Dave Ely <dely@dijatool.com>
2012-09-06 22:33:06 -0700

    Added roster-scraper.py.
    Still not sure how to properly follow the tags without clear class embedding.

---
+++ roster-scraper.py
@@ -0,0 +1,43 @@
+
+
+_docs = """
+
+Need a scraper for this page so I know if something changed
+
+http://www.packers.com/team/players.html
+
+classes:
+	game-roster mod-tabular
+		game-roster-large
+			bd
+
+
+mod-title-nobackground
+	these are the segments...
+then
+table
+	tbody
+		tr class=loop-*
+
+<tr class="loop-first  loop-odd">
+        <td class="col-jersey" rel="67">
+            67
+        </td>
+        <td class="col-name">
+            
+                
+                
+                    
+                
+            
+            <a href="/team/roster/Don-Barclay/4c69f318-aedc-4830-8c13-0c5e9810e5cc" rel="/cda-web/person-card-module.htm?mode=data&amp;id=4c69f318-aedc-4830-8c13-0c5e9810e5cc" rev="Player" class="player-card-tooltip" title="Barclay, Don" onclick="s_objectID=&quot;http://www.packers.com/team/roster/Don-Barclay/4c69f318-aedc-4830-8c13-0c5e9810e5cc_1&quot;;return this.s_oc?this.s_oc(e):true"><span>Barclay, Don</span></a>
+        </td>
+        <td class="col-position">G/T</td>
+        <td class="col-height">6-4</td>
+        <td class="col-weight">305</td><td class="col-bd">23</td><td class="col-exp" rel="00">
+            R
+        </td>
+        <td class="col-college">West Virginia</td>
+    </tr>
+"""
+
