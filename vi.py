b03e7f1484d148559f376c434a8d30051efea512
Dave Ely <dely@dijatool.com>
2012-12-23 17:10:08 -0800

    Pull odds from vegasinsider.com

---
+++ vi.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from odds import teamsTranslate
+from misc import cleanText
+
+import urllib2
+
+from BeautifulSoup import BeautifulSoup as bs
+
+#
+# 	While debugging...
+#
+# 	python ~/github/local/odds-scraper/vi.py
+#
+
+
+#
+#	So the page is gzip'd, created a quick work around for it
+#
+#	FeedParser does this better or so I read...
+#	http://code.google.com/p/feedparser/
+#
+# 	git clone https://code.google.com/p/feedparser/
+#
+#
+#
+
+try:
+    from io import BytesIO as _StringIO
+except ImportError:
+    try:
+        from cStringIO import StringIO as _StringIO
+    except ImportError:
+        from StringIO import StringIO as _StringIO
+
+
+def fixOdds( awayOdds, homeOdds ) :
+	'''
+		fixOdds needs a description...
+
+	'''
+	awayOdds = cleanText( awayOdds )
+	homeOdds = cleanText( homeOdds )
+	if '-' == awayOdds[ 0 ] :
+		awayOdds = awayOdds.split( " " )[0]
+		homeOdds = awayOdds[ 1 : ]
+	else :
+		homeOdds = homeOdds.split( " " )[0]
+		awayOdds = homeOdds[ 1 : ]
+
+	return awayOdds, homeOdds
+
+
+def main() :
+	'''
+		Pull the page and parse it into the pieces we need.
+
+			td class="viBodyBorderNorm"
+			find the second table...
+				find all the tr's
+
+	'''
+	from optparse import OptionParser
+
+	usage = "%prog [options]"
+	parser = OptionParser( usage = usage )
+	parser.add_option( "-e", "--extendedOdds", dest="extendedOdds", default = False,
+						action="store_true",
+						help="Determine if we show all the extra data." )
+
+	( options, args ) = parser.parse_args()
+
+	url = "http://www.vegasinsider.com/nfl/odds/las-vegas/"
+ 	# url = "http://localhost/odds.html"
+	opener = urllib2.build_opener()
+	response = opener.open( url )
+
+	page = None
+
+	if response.info().get( 'Content-Encoding' ) == 'gzip' :
+		import gzip
+		f = gzip.GzipFile( fileobj=_StringIO( response.read() ))
+		page = f.read()
+	else :
+		page = response.read()
+	soup = bs( page )
+
+	mainTable = soup.findChild( 'td', { "class" : "viBodyBorderNorm" })
+	tables = mainTable.findAll( 'table' )
+
+	oddsTable = tables[ 1 ]
+
+	rows = oddsTable.findAll( 'tr' )
+
+	for aRow in rows :
+		#print aRow
+		try :
+			teams = aRow.findChildren( 'a', { "class" : "tabletext" })
+			if None != teams and len( teams )  > 0 :
+				visitor = teams[ 0 ].getText( " " )
+				home = teams[ 1 ].getText( " " )
+				( visitor, home ) = teamsTranslate( visitor, home )
+
+				# find the proper column
+				cols = aRow.findAll( 'td' )
+
+				viOdds = cols[ 2 ].findChild( 'a' )
+				odssItems = viOdds.getText( "|" ).split( "|" )
+				awayOdds, homeOdds = fixOdds( odssItems[ 1 ], odssItems[ 2 ] )
+
+				# print "+++++++++++++++++++++++++++++++++"
+				# print
+				print visitor, "@", home, homeOdds
+
+
+		except Exception as ex :
+			print "We had an exception!", ex
+
+
+if __name__ == '__main__':
+	main()
+
+
